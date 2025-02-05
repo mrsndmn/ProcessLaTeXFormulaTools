6b9f37b759142626cd2404b96b0bf3340d615bd7
Dmitrii Tarasov <d.tarasov@airi.net>
2025-02-05 21:42:31 +0300

    fix

--- process_formula/preprocess_latex.js
+++ process_formula/preprocess_latex.js
@@ -1,5 +1,5 @@
 
-var katex = require(import.meta.filename.replace('preprocess_latex.js', '') + 'third-party/katex/dist/katex.js')
+var katex = require('./third-party/katex/katex_dist/katex.js')
 
 var readline = require('readline');
 
